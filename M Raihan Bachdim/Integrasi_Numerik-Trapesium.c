fa1088399d8a8169c05ff3f58d3c7f790c32b852
Raihan Bachdim <115518716+raihanbachdim@users.noreply.github.com>
2023-01-03 09:53:33 +0700

    Update Integrasi_Numerik-Trapesium.c

--- M Raihan Bachdim/Integrasi_Numerik-Trapesium.c
+++ M Raihan Bachdim/Integrasi_Numerik-Trapesium.c
@@ -2,15 +2,16 @@
 #include<conio.h>
 #include<math.h>
 
-/* Define function here */
+// Deklarasikan fungsi disini
 #define f(x) 2*x+3
 //x/(1+pow(x,2))
 
 int main(){
-    float a, b, integrasi=0.0, pias, k,s;
-    int i, nInterval;
-    // clrscr();
-    /* Input */
+	// Mendeklarasikan variabel yang akan digunakan
+	float a, b, integrasi=0.0, pias, k,s;
+	int i, nInterval;
+	
+	// Mengambil inputan user, kemudian disimpan dalam variabel
 	printf("Masukan batas bawah integral : ");
 	scanf("%f", &a);
 	printf("Masukan batas atas integral : ");
@@ -18,23 +19,31 @@ int main(){
 	printf("Enter number of sub intervals: ");
 	scanf("%d", &nInterval);
 
-    /* Calculation */
-    /* Finding step size */
-    pias = (b - a)/nInterval;
+	// Proses kalkulasi
+	
+	// Menghitung panjang satu pias
+	pias = (b - a)/nInterval;
 
-    /* Finding integrasi Value */
-    integrasi = f(a) + f(b);
-    
-    printf("\n %.3f", integrasi);
-    for(i=1; i<= nInterval-1; i++){
-        k = a + i*pias;
-        printf("\n %.3f , %.3f", k, f(k));
-        s = f(k);
-        integrasi +=  2 * s;
-        printf("\n %.3f", integrasi);
-    }
-    integrasi = integrasi * pias/2;
-    printf("\nNilai hampiran dari integrasi adalah : %.3f", integrasi);
-    getch();
-    return 0;
+	// Menghitung nilai fungsi dari bagian awal dan akhir integral 
+	integrasi = f(a) + f(b);
+	
+	// Comment dibawah digunakan jika ingin mencetak hasil perhitungan jumlah integrasi dari fungsi awal dan akhir 
+	// printf("\n %.3f", integrasi);
+	
+	// Pengulangan untuk menghitung setiap jumlah fungsi setiap pias
+	for(i=1; i<= nInterval-1; i++){
+		k = a + i*pias;
+		//mencetak nilai integrasi setiap pengulangan 	
+		printf("\n %.3f , %.3f", k, f(k));
+		
+		// variabel s digunakan untuk menampung jumlah fungsi dari perhitungan sebelumnya
+		s = f(k);
+		integrasi +=  2 * s;	
+	}
+	
+	// Menghitung nilai akhir dari total integrasi	
+	integrasi = integrasi * pias/2;
+	printf("\nNilai hampiran dari integrasi adalah : %.3f", integrasi);
+	getch();
+	return 0;
 }
